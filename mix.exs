23b25f33c3123863d97889620b1083c7fbbe4e53
Nathan Hessler <xmilestegx@gmail.com>
2016-04-03 19:52:00 -0400

    generate doc pages

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Issues.Mixfile do
     [app: :issues,
      escript: escript_config,
      version: "0.0.1",
+     name: "Issues",
+     source_url: "https://github.com/nhessler/issues",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -30,7 +32,9 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.8"},
-      {:poison,    "~> 1.5"}
+      {:poison,    "~> 1.5"},
+      {:ex_doc,    "~> 0.11"},
+      {:earmark,   ">= 0.0.0"}
     ]
   end
 
